0215ebc79a5ef55829d061c21ba0bd341b1b6101
Lultik <mihwil27021@gmail.com>
2020-11-09 22:44:32 +0300

    feat: complete task

--- js/main.js
+++ js/main.js
@@ -49,6 +49,55 @@ function setBgGreet() {
    }
 }
 
+// Get Name
+function getName() {
+   if (localStorage.getItem('name') === null) {
+      name.textContent = '[Enter Name]';
+   } else {
+      name.textContent = localStorage.getItem('name');
+   }
+}
+
+// Set Name
+function setName(e) {
+   if (e.type === 'keypress') {
+      // Make sure Enter is pressed
+      if(e.keyCode == 13) {
+         localStorage.setItem('name', e.target.innerText);
+         name.blur();
+      }
+   } else {
+      localStorage.setItem('name', e.target.innerText);
+   }
+}
+
+function getFocus() {
+   if (localStorage.getItem('focus') === null) {
+      focus.textContent = '[Enter Focus]';
+   } else {
+      focus.textContent = localStorage.getItem('focus');
+   }
+}
+
+function setFocus(e) {
+   if (e.type === 'keypress') {
+      // Make sure Enter is pressed
+      if(e.keyCode == 13) {
+         localStorage.setItem('focus', e.target.innerText);
+         focus.blur();
+      }
+   } else {
+      localStorage.setItem('focus', e.target.innerText);
+   }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
 // Run
 showTime();
 setBgGreet();
+getName();
+getFocus();
